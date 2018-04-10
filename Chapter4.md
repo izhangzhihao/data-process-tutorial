8a4cabbe84aa3d4a1b91d4214ae2a2915b3ec4be
张志豪 <izhangzhihao@hotmail.com>
2018-04-10 16:49:56 +0800

    Stage changes of BatchProcess.scala

---
+++ Chapter4.md
@@ -0,0 +1,3 @@
+## Chapter 4: Batch Processing In Action
+
+### What is batch processing
--- src/main/scala/com/github/izhangzhihao/BatchProcess.scala
+++ src/main/scala/com/github/izhangzhihao/BatchProcess.scala
@@ -1,7 +1,13 @@
 package com.github.izhangzhihao
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
+/**
+  * Data taken from <a href="http://archive.ics.uci.edu/ml/datasets/Online+Retail"/>
+  * This is a transnational data set which contains all the transactions occurring
+  * between 01/12/2010 and 09/12/2011 for a UK-based and registered non-store online retail.
+  * The company mainly sells unique all-occasion gifts. Many customers of the company are wholesalers.
+  */
 object BatchProcess extends App {
 
   val spark = SparkSession.builder()
@@ -10,7 +16,6 @@ object BatchProcess extends App {
     .getOrCreate()
 
   import spark.implicits._
-
   import org.apache.spark.sql.functions._
   import org.apache.spark.sql.types._
 
@@ -19,7 +24,7 @@ object BatchProcess extends App {
     .option("inferSchema", "true")
     .option("header", "true")
     .csv("data/retail-data/by-day/*.csv")
-    .repartition(2)
+    .repartition(4)
 
   originData.printSchema()
 
@@ -34,12 +39,64 @@ object BatchProcess extends App {
       $"CustomerID".cast("int"),
       $"Country"
     )
-    .filter("CustomerID is not null") // where === filter
+    .filter('Country === "United Kingdom") //restrict the data to only United Kingdom customers, which contains most of our customers historical data.
+    .filter($"Quantity" > 0) //remove canceled orders
+    .filter('InvoiceDate gt lit("2010-12-09")) // restrict the data to one full year because it's better to use a metric per Months or Years
+    .where("CustomerID is not null") //remove rows where customerID are NA
 
 
   retailData.printSchema()
 
-  retailData.show(10)
+  retailData.show()
 
-  retailData.write.json("data/retail-data/all/online-retail-dataset")
+  retailData.createOrReplaceTempView("retail")
+
+  spark.sql(
+    """
+                 SELECT COUNT(DISTINCT InvoiceNo) as Number_of_transactions FROM retail
+      """).show()
+
+  spark.sql(
+    """
+                 SELECT COUNT(DISTINCT StockCode) as Number_of_products_bought FROM retail
+      """).show()
+
+  spark.sql(
+    """
+                 SELECT COUNT(DISTINCT CustomerID) as Number_of_customers FROM retail
+      """).show()
+
+  retailData.select(countDistinct('CustomerID).as("Number of customers")).show()
+
+  retailData.select(max('InvoiceDate) as "last date").show()
+
+  val retailDataWithDate: DataFrame = retailData.withColumn("date", to_date('InvoiceDate))
+  retailDataWithDate.show(5)
+
+
+  val retailDateWithLasePurchaseDate: Dataset[Row] = retailDataWithDate
+    .groupBy('CustomerID)
+    .agg(max('date) as "LastPurchaseDate")
+
+  retailDateWithLasePurchaseDate.show()
+
+  //Frequency helps us to know how many times a customer purchased from us.
+  //To do that we need to check how many invoices are registered by the same customer.
+
+  retailDateWithLasePurchaseDate
+    .withColumn("today", to_date(expr(""""2011-12-09"""")))
+    .withColumn("Recency", datediff('today, 'LastPurchaseDate))
+    .show()
+
+  //Monetary attribute answers the question: How much money did the customer spent over time?
+
+
+  retailData
+    .withColumn("TotalCost", expr("Quantity * UnitPrice"))
+    .groupBy('CustomerID)
+    .sum("TotalCost")
+    .show()
+
+
+  //retailData.write.json("data/retail-data/all/online-retail-dataset")
 }
