726e8d09343f92cdb75b81be44fee5191be28650
张志豪 <izhangzhihao@hotmail.com>
2018-04-10 22:04:14 +0800

    Applying RFM score formula (WIP)

--- src/main/scala/com/github/izhangzhihao/BatchProcess.scala
+++ src/main/scala/com/github/izhangzhihao/BatchProcess.scala
@@ -1,6 +1,6 @@
 package com.github.izhangzhihao
 
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql._
 
 /**
   * Data taken from <a href="http://archive.ics.uci.edu/ml/datasets/Online+Retail"/>
@@ -82,11 +82,10 @@ object BatchProcess extends App {
   //To calculate recency, we need to choose a date point from which we evaluate how many days ago was the customer's last purchase.
 
   val retailDataWithRecency: DataFrame = retailDataWithLasePurchaseDate
-    .withColumn("today", to_date(expr(""""2011-12-09"""")))
+    .withColumn("today", to_date(expr(""""2011-12-09""""))) //Should be simplified
     .withColumn("Recency", datediff('today, 'LastPurchaseDate))
     .drop("today", "LastPurchaseDate")
-  retailDataWithRecency
-    .show()
+  retailDataWithRecency.show()
 
   //Frequency helps us to know how many times a customer purchased from us.
   //To do that we need to check how many invoices are registered by the same customer.
@@ -96,8 +95,7 @@ object BatchProcess extends App {
     .count()
     .withColumnRenamed("count", "Frequency")
 
-  retailDataWithFrequency
-    .show()
+  retailDataWithFrequency.show()
 
 
   //Monetary attribute answers the question: How much money did the customer spent over time?
@@ -139,6 +137,84 @@ object BatchProcess extends App {
 
   // 5,646,700.091200001 VS 5,158,179.370999999
   //In our case, the 80% of total revenue is not achieved by the 20% of TOP customers but approximately, it does, because they are less than our 20% TOP customers who achieve it. It would be interesting to study this group of customers because they are those who make our most revenue.
+
+  //Applying RFM score formula
+
+  private def getQuartiles(col: String) = {
+    Array(0.25, 0.50, 0.75).zip(RFMTable.stat.approxQuantile(col, Array(0.25, 0.50, 0.75), 0.05)).toMap
+  }
+
+  val recencyQuartiles = getQuartiles("Recency")
+  val frequencyQuartiles = getQuartiles("Frequency")
+  val monetaryQuartiles = getQuartiles("Monetary")
+
+  object UDFUtils extends Serializable {
+
+    //We will create two segmentation classes since, high recency is bad, while high frequency and monetary value is good.
+
+    /**
+      * For recency
+      *
+      * @param value
+      * @param dict
+      * @return
+      */
+    def RScore(value: Double, dict: Map[Double, Double]) =
+      value match {
+        case v if v <= dict(0.25) => 4
+        case v if v <= dict(0.50) => 3
+        case v if v <= dict(0.75) => 2
+        case _ => 1
+      }
+
+    /**
+      * For frequency and monetary
+      *
+      * @param value
+      * @param dict
+      * @return
+      */
+    def FMScore(value: Double, dict: Map[Double, Double]) =
+      value match {
+        case v if v <= dict(0.25) => 1
+        case v if v <= dict(0.50) => 2
+        case v if v <= dict(0.75) => 3
+        case _ => 4
+      }
+
+
+    def R_Quartile(value: Double) = {
+      RScore(value, recencyQuartiles)
+    }
+
+    //DSL : Usage
+    val R_QuartileUDF = udf[Int, Double](R_Quartile)
+
+    // Works With Sql Expr and DSL API
+    val R_QuartileUDFRegister = spark.udf.register("R_Quartile", R_QuartileUDF)
+
+    def F_Quartile(value: Double) = {
+      FMScore(value, frequencyQuartiles)
+    }
+
+    val F_QuartileUDF = udf[Int, Double](F_Quartile)
+    val F_QuartileUDFRegister = spark.udf.register("F_Quartile", F_QuartileUDF)
+
+    def M_Quartile(value: Double) = {
+      FMScore(value, monetaryQuartiles)
+    }
+
+    val M_QuartileUDF = udf[Int, Double](M_Quartile)
+    val M_QuartileUDFRegister = spark.udf.register("M_Quartile", M_QuartileUDF)
+  }
+
+  import UDFUtils._
+
+  RFMTable
+    .withColumn("R_Quartile", R_QuartileUDF('Recency))
+    .withColumn("F_Quartile", expr("F_Quartile(Frequency)"))
+    .withColumn("M_Quartile", callUDF("M_Quartile", 'Monetary))
+    .show()
 
 
   //retailData.write.json("data/retail-data/all/online-retail-dataset")
